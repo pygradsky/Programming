46a2422a97f83d17021fa3de33915c5afa22cd52
Fedor <feda.vert@gmail.com>
2026-02-18 11:16:32 +0300

    Create boolean.cpp

---
+++ practise/boolean.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    bool a = true;
+    bool b = false;
+    
+    cout << "a && b = " << (a && b) << endl;
+    cout << "a || b = " << (a || b) << endl;
+    cout << "!a = " << (!a) << endl;
+    
+    return 0;
+}
