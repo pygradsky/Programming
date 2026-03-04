224aaff49f699b6b3d288c27ec42e7de8d8e71ca
Fedor <feda.vert@gmail.com>
2026-03-04 11:31:50 +0300

    Update sinx_cosx.cpp

--- works/sinx_cosx.cpp
+++ works/sinx_cosx.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+double calculateSinX(double* x) {
+    return sin(*x);
+}
+
+double calculateCosX(double* x) {
+    return cos(*x);
+}
+
+double calculateTanX(double* x) {
+    return sin(*x) / cos(*x);
+}
+
+int main() {
+    double x;
+    
+    cout << "Введите значение x в радианах: ";
+    cin >> x;
+    
+    cout << "sin(x) = " << calculateSinX(&x) << endl;
+    cout << "cos(x) = " << calculateCosX(&x) << endl;
+    cout << "tan(x) = sin(x)/cos(x) = " << calculateTanX(&x) << endl;
+    
+    return 0;
+}
